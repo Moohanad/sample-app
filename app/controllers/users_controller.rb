73275b6477fb69341f7f62ebc52371da6c2a10cc
Moohanad <moohrash@yahoo.com>
2012-03-14 13:20:11 -0700

    branch

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  def new
+	@title = "Sign up"
+	@user = User.new
+  end
+  def show
+    @user = User.find(params[:id])
+    @title = "Sign up"
+  end
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+	  sign_in @user
+	  flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      @title = "Sign up"
+      render 'new'
+    end
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,23 @@
+SampleApp::Application.routes.draw do
+  get "sessions/new"
+  resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
+
+  match '/signup',  :to => 'users#new'
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
+  
+  resources :users
+  match '/signup',  :to => 'users#new'
+
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+
+  root :to => 'pages#home'
+
+  get "pages/home"
+  get "pages/contact"
+  get "pages/about"
+  
+  end
